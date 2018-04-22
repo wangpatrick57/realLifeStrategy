a9ec5734d13e66637006d4843d0220e8283ad7d9
Melody <melody20011102@gmail.com>
2018-04-22 02:00:30 -0700

    i

--- GameMap/GameMap/ViewController.swift
+++ GameMap/GameMap/ViewController.swift
@@ -13,29 +13,25 @@ import MapKit
 
 class ViewController: UIViewController {
 
-    // MARK: Properties
-    
-    @IBOutlet weak var mapView: MKMapView!
-    
-    override func viewDidLoad() {
+     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let initialLocation = CLLocation(latitude: 122.0322, longitude: 37.3230)
-        centerMapOnLocation(location: initialLocation)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
     
-    let regionRadius: CLLocationDistance = 1000
-    func centerMapOnLocation(location: CLLocation) {
-        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius, regionRadius)
-        mapView.setRegion(coordinateRegion, animated: true)
+    // MARK: Properties
+    @IBOutlet weak var mapView: MKMapView!
+    
+    //MARK: Actions
+    @IBAction func touchExit(_ sender: UITapGestureRecognizer) {
+        print("touched")
     }
-
-
+    @IBAction func gameIDTF(_ textField: UITextField) {
+    }
 }
 
 
