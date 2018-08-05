74011e93c83b99bfcbc241d028204c987d572838
fishgoatman <wang.patrick57@gmail.com>
2018-08-05 09:12:56 -0700

    added hosting and joining game

--- RLS/RLS/HostOrJoinViewController.swift
+++ RLS/RLS/HostOrJoinViewController.swift
@@ -6,11 +6,17 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseFirestore
 
-class HostOrJoinViewController : UIViewController{
+var gameId:String = "generating"
+let gameIdLength:Int = 5
+let db = Firestore.firestore()
+
+class HostOrJoinViewController : UIViewController {
     @IBAction func HostButton(_ sender: Any) {
         print("Host Button clicked")
-        self.performSegue(withIdentifier: "HostGameIDSegue", sender: self)
+        checkIdTaken()
     }
     
     @IBAction func JoinButton(_ sender: Any) {
@@ -20,11 +26,57 @@ class HostOrJoinViewController : UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func checkIdTaken() {
+        gameId = generateGameId()
+        let docRef:DocumentReference = db.document("Games/" + gameId)
+        
+        docRef.getDocument { (document, error) in
+            if let document = document {
+                if document.exists {
+                    print(gameId + " taken")
+                    self.checkIdTaken()
+                } else {
+                    db.document("Games/" + gameId).setData([
+                        "test": "test"
+                    ]) { err in
+                        if let err = err {
+                            print("Error writing document: \(err)")
+                        } else {
+                            print("Document successfully written!")
+                        }
+                    }
+                    
+                    print(gameId + " not taken")
+                    self.performSegue(withIdentifier: "HostGameIDSegue", sender: self)
+                }
+            }
+        }
+    }
+    
+    func generateGameId()->String {
+        var gameId:String = ""
+        let alphabet: [String] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+        
+        for i in 1...gameIdLength {
+            let rand:Int = Int(arc4random_uniform(36))
+            var add:String
+            
+            if (rand < 10) {
+                add = String(rand)
+            } else {
+                add = alphabet[rand - 10]
+            }
+            
+            gameId += add
+        }
+        
+        return gameId
     }
 }
