9e6208c5993505e110593e726d199b54986003e5
Ethan Soo <ethan.soo@hotmail.com>
2019-05-08 11:46:30 -0700

    Added getDistance and getClosestPlayer

--- RLS/RLS/PlayerDistanceUtil.swift
+++ RLS/RLS/PlayerDistanceUtil.swift
@@ -10,16 +10,36 @@ import Foundation
 
 class PlayerDistanceUtil {
     
-    static func getDistance(_playerX: Player, _playerY: Player) -> Double{
-        let latDifference: Double = _playerX.getCoordinate().latitude - _playerY.getCoordinate().latitude
-        let longDifference: Double = _playerX.getCoordinate().longitude - _playerY.getCoordinate().longitude
+    //param: player A, player B
+    //return distance between both players
+    static func getDistance(_ playerX: Player, _ playerY: Player) -> Double{
+        let latDifference: Double = playerX.getCoordinate().latitude - playerY.getCoordinate().latitude
+        let longDifference: Double = playerX.getCoordinate().longitude - playerY.getCoordinate().longitude
         
         let dist = abs(sqrt(pow(latDifference, 2) + pow(longDifference, 2)))
         
         return dist
     }
     
-    static func getClosestPlayer() -> Player{
-        return Player()
+    //param: main player, list of all players in game
+    //return player closest to main player
+    static func getClosestPlayer(mainPlayer: Player, playerList: [Player]) -> Player{
+        
+        var closestDistance: Double = getDistance(mainPlayer, playerList[0])
+        var closestPlayer: Player = playerList[0]
+        var distance: Double
+        
+        for i in 1...playerList.count {
+            
+            distance = getDistance(mainPlayer, playerList[i])
+            
+            if (playerList[i] != mainPlayer && distance < closestDistance){
+                closestDistance = distance
+                closestPlayer = playerList[i]
+            }
+            
+        }
+        return closestPlayer
     }
+    
 }
