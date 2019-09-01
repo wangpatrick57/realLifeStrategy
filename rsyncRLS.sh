96b47ce053ad77928be7f7796545ad1af62d611f
Melody Lee <melodylee@Melodys-MacBook-Pro.local>
2019-09-01 13:42:26 -0400

    Merge

--- rsyncRLS.sh
+++ rsyncRLS.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
-rsync -avz --progress -e 'ssh -p 22' rlsServer/ local@73.189.41.182:/home/local/rlsServer/
+rsync -avz --progress -e 'ssh -p 22' stableServer/ local@73.189.41.182:/home/local/stableServer/
+rsync -avz --progress -e 'ssh -p 22' testServer/ local\
+@73.189.41.182:/home/local/testServer/
